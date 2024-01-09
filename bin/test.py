99a85e7a739e8fd8bb5735d5d96aaa130e2fa141
skusch <skusch@bio1.rwth-aachen.de>
2024-01-09 14:47:56 +0100

    Update test.py

--- bin/test.py
+++ bin/test.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from TE_Trimmer import main  # Import your main function
+from TE_Trimmer import main  # Import the main function
 from click.testing import CliRunner
 import click
 
@@ -15,9 +15,9 @@ class TestMainFunction(unittest.TestCase):
         os.makedirs(output_dir, exist_ok=True)
         if os.listdir(output_dir):
             click.echo(
-                f"WARNING: The output directory {output_dir} is not empty. Please empty your output directory or "
-                f"choose another empty directory\n")
-            # Stop the whole program when the output directory is not empty
+                f"WARNING: The output directory {output_dir} is not empty. Please empty the output directory or "
+                f"choose another empty directory.\n")
+            # Stop the program if the output directory is not empty
             return
         
         args = [
@@ -32,22 +32,22 @@ class TestMainFunction(unittest.TestCase):
         print(result.output)
 
         # Add assertions to check the results
-        # check input fasta
+        # Check input FASTA file
         files_in_folder = os.listdir(f"{output_dir}/Single_fasta_files")
         expected_file_count = 8
-        self.assertEqual(len(files_in_folder), expected_file_count, f"Expected {expected_file_count} files in the {output_dir}/Single_fasta_files, but found {len(files_in_folder)} files.")
+        self.assertEqual(len(files_in_folder), expected_file_count, f"Expected {expected_file_count} files in the directory {output_dir}/Single_fasta_files, but found {len(files_in_folder)} files.")
 
-        # check summary.txt
-        self.assertTrue(os.path.exists('test_output_dir/summary.txt'), 'summary.txt not found')
+        # Check summary.txt
+        self.assertTrue(os.path.exists('test_output_dir/summary.txt'), 'summary.txt not found.')
         summary_size = os.path.getsize(os.path.join(output_dir, 'summary.txt'))
-        self.assertGreater(summary_size, 800, 'summary.txt is not complete')
+        self.assertGreater(summary_size, 800, 'summary.txt is not complete.')
 
-        # check TE_Trimmer_consensus.fasta
-        self.assertTrue(os.path.exists('test_output_dir/TE_Trimmer_consensus.fasta'), 'TE_Trimmer_consensus.fasta not found')
+        # Check TE_Trimmer_consensus.fasta
+        self.assertTrue(os.path.exists('test_output_dir/TE_Trimmer_consensus.fasta'), 'TE_Trimmer_consensus.fasta not found.')
         te_trimmer_size = os.path.getsize(os.path.join(output_dir, 'TE_Trimmer_consensus.fasta'))
-        self.assertGreater(te_trimmer_size, 10000, 'TE_Trimmer_consensus.fasta is not complete')
+        self.assertGreater(te_trimmer_size, 10000, 'TE_Trimmer_consensus.fasta is not complete.')
 
-        # check proof_annotation
+        # Check proof_annotation directory
         contents = os.listdir(f"{output_dir}/TE_Trimmer_for_proof_annotation/Need_check_annotation")
         self.assertGreater(len(contents), 0, f"{output_dir}/TE_Trimmer_for_proof_annotation/Need_check_annotation is empty.")
 
