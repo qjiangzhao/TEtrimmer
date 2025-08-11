54df7892cbe5a1678ad2b8403b6e5be3f6a39e3b
Jiangzhao_Qian <jqian@bio1.rwth-aachen.de>
2025-08-11 14:48:05 +0200

    updated tetrimmer verstion to 1.5.2

--- tetrimmerGUI/TEtrimmerGUI.py
+++ tetrimmerGUI/TEtrimmerGUI.py
@@ -184,7 +184,7 @@ if os_type == "Windows":
     type=str,
     help='Log level. [DEBUG, INFO, WARNING, ERROR, CRITICAL]',
 )
-@click.version_option("1.5.1", prog_name='TEtrimmerGUI')
+@click.version_option("1.5.2", prog_name='TEtrimmerGUI')
 def proof_curation(
     te_trimmer_proof_curation_dir,
     output_dir,
