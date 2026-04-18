191ccac1d60a5db3deebee416d518ddaa837090a
Jiangzhao_Qian <jqian@bio1.rwth-aachen.de>
2026-04-18 18:12:37 +0200

    improved TEAid plots

--- tetrimmer/TE-Aid-master/blastndotplot.R
+++ tetrimmer/TE-Aid-master/blastndotplot.R
@@ -1,110 +1,133 @@
-
 # self sequence db is being made by the shell script // or you need to make it to use in R
-blastdotplot=function(query = NULL, db = NULL, blast = NULL, os = NULL, tables = NULL, output = NULL){
+blastdotplot <- function(query = NULL,
+                         db = NULL,
+                         blast = NULL,
+                         os = NULL,
+                         tables = NULL,
+                         output = NULL) {
   
-  # run the selfblast
-  bl=read.table(text=system(paste("blastn -max_target_seqs 10000 -query", query, "-db", db, "-evalue 0.05 -outfmt 6 -word_size 11 -gapopen 5 -gapextend 2 -reward 2 -penalty -3 | cut -f 1,7-10 | sed 's/#/-/g'"),
-                            intern = TRUE)
-                )
-  # order from left to right
-  bl=bl[order(bl$V2, decreasing = F),]
+  # Run the selfblast
+  bl <- read.table(
+    text = system(
+      paste(
+        "blastn -max_target_seqs 10000 -query", query,
+        "-db", db, "-evalue 0.05 -outfmt 6 -word_size 11",
+        "-gapopen 5 -gapextend 2 -reward 2 -penalty -3 | cut -f 1,7-10 | sed 's/#/-/g'"
+      ),
+      intern = TRUE
+    )
+  )
   
-  # test if there are orf detected; will later store in orf if TRUE
+  # Order from left to right
+  bl <- bl[order(bl$V2, decreasing = FALSE), ]
+
+  # ONLY take the top 17 hits
+  if (nrow(bl) > 17) {
+    bl <- bl[1:17, ]
+  }
+  
+  # Test if there are ORF detected; will later store in orf if TRUE
   if (exists("blast") && file.exists(as.character(blast))) {
-  test <- try(read.table(as.character(blast)), silent = TRUE)
+    test <- try(read.table(as.character(blast)), silent = TRUE)
+  } else {
+    test <- FALSE
   }
-  else {
-  test <- FALSE
+  
+  if (class(test) == "data.frame") {
+    orfs <- read.table(as.character(blast))
+  } else {
+    print("no orf to plot...")
+    orfs <- suppressWarnings(as.data.frame(0))
+    names(orfs) <- "V1"
+    # Note: V2 is intentionally missing here to trigger the safe 'has_data' check
   }
-
-  # test if there are TE prot detected; will later store in prot if TRUE
-  #test2<-try(read.table(as.character(blast)), T)
-
-  if(class(test) == "data.frame"){
-     orfs=read.table(as.character(blast))
-     } else {
-     print("no orf to plot...")
-     orfs <- suppressWarnings(as.data.frame(0))
-     names(orfs)<-"V1"
-   }
-
-  # if(class(test2) == "data.frame"){
-  #    orfs=read.table(as.character(getorf))
-  #    }else{
-  #    suppressWarnings(prot$V1 <- as.data.frame(c(0,0))[,1])
-  #  }
+  
   ###########################
   ## dot-plot (bottom left)##
   ###########################
-
-  plot(x = 1, type = "n", xlim = c(0,bl$V3[1]),ylim = c(0,bl$V3[1]), col = "white",
-       main = "",
-       ylab = "TE consensus self dotplot (bp)",
-       xlab = "TE consensus self dotplot (bp)",
-       cex.lab = 2,
-       cex.axis = 1.5,
-       xaxs = "i", yaxs = "i"
-       )
-    for(i in 1:length(bl$V1)){
-      if(bl$V5[i] > bl$V4[i]){
-        segments(x0 = bl$V2[i], x1 = bl$V3[i], y0 = bl$V4[i], y1 = bl$V5[i], col = "black", lwd = 1.5)
-      } else {
-        segments(x0 = bl$V2[i], x1 = bl$V3[i], y0 = bl$V4[i], y1 = bl$V5[i], col = "#009E73", lwd = 1.5)  
-      }
-        # if orientation
-    } # for each segment end
+  
+  plot(
+    x = 1, type = "n",
+    xlim = c(0, bl$V3[1]), ylim = c(0, bl$V3[1]),
+    col = "white", main = "",
+    ylab = "TE consensus self dotplot (bp)",
+    xlab = "TE consensus self dotplot (bp)",
+    cex.lab = 2, cex.axis = 1.5, xaxs = "i", yaxs = "i"
+  )
+  
+  for (i in 1:length(bl$V1)) {
+    if (bl$V5[i] > bl$V4[i]) {
+      segments(x0 = bl$V2[i], x1 = bl$V3[i], y0 = bl$V4[i], y1 = bl$V5[i], col = "black", lwd = 1.5)
+    } else {
+      segments(x0 = bl$V2[i], x1 = bl$V3[i], y0 = bl$V4[i], y1 = bl$V5[i], col = "#009E73", lwd = 1.5)
+    }
+  } 
   
   #####################################
   ## Annotation graph (bottom right) ##
   #####################################
-
-  ## Arrows layer ##
-
-  plot(x = 1, type = "n", xlim = c(0,bl$V3[1]),ylim = c(-max(length(orfs$V1),10),length(bl$V1)), col = "white", yaxt="n",
-       main = "",
-       xlab = "TE consensus structure and protein hits (bp)",
-       ylab = "",
-       cex.lab = 2,
-       cex.axis = 1.5,
-       xaxs = "i", yaxs = "i"
-  )
-  for(i in seq(1:length(bl$V1))){
-    if(bl$V4[i] > bl$V2[i]){
-        arrows(x0 = bl$V2[i], x1 = bl$V3[i], y0 = i, y1 = i, col = rainbow(length(bl$V1))[i], lwd = 3, length = 0.1)
-        arrows(x0 = bl$V4[i], x1 = bl$V5[i], y0 = i, y1 = i, col = rainbow(length(bl$V1))[i], lwd = 3, length = 0.1)
-    } # if to draw (filter)
-  } # for each segment end
-
-
-  ## Orfs layers ##
-  if(class(test) != "data.frame"){ # if orf table is empty
-     text("No TE domain detected", x=bl$V3[1]/2, y=-5, cex = 2)
-    } else { # if ORF table, plot orfs
-      for(i in seq(1:length(orfs$V1))){
-        if(orfs$V1[i] < orfs$V2[i]){ # checking ORF orientation
-          rect(xleft = orfs$V1[i], xright = orfs$V2[i], # draw a + ORF
-               ybottom = -i-0.15, ytop = -i+0.15, lwd = 1, border = "black")
-        } else {
-          rect(xleft = orfs$V1[i], xright = orfs$V2[i], # draw a - ORF
-               ybottom = -i-0.15, ytop = -i+0.15, lwd = 1, border = "red")  
-        } # orientation
-
-          ## TE protein hits (blastp) ##
-          rect(xleft = orfs$V5[i], xright = orfs$V6[i], 
-               ybottom = -i-0.13, ytop = -i+0.13, lwd = 1, col = as.character(paste("#",orfs$V9[i], sep="")), border = NA) # draw colored rectangle same way as orf
-          text(paste("(",orfs$V8[i],")", orfs$V3[i], orfs$V4[i]), x = (min(orfs$V1[i],orfs$V2[i])+max(orfs$V1[i],orfs$V2[i]))/2, y = -i+0.15, pos = 3) # print hit name
-      
-        } # for each segment
-      #names(orfs)<-c("orf.start", "orf.end", "hit.TE.prot", "TE.Class", "hit.start", "hit.end", "strand", "color")
-      #print(orfs)
-  } # if orf present plot orfs and prot hits
-    
-  #print(tables)
-  if(tables == "TRUE"){
-    names(bl)<-c("TE", "from.1", "to.1", "from.2", "to.2")
-    write.table(bl, file = paste(output, "/TE.self-blast.txt", sep = ""), quote = F, row.names = F)
-  } # if tables to print
   
-} # function end
-
-
+  plot_te_structure <- function(orfs, bl) {
+    # FIX: Ensure V2 exists to prevent "argument is of length zero" error
+    has_data <- is.data.frame(orfs) && nrow(orfs) > 0 && !is.null(orfs$V2)
+    
+    # Constants for visual consistency
+    row_height <- 1
+    bar_thickness <- 0.25 
+    
+    # Logic to prevent "fat" boxes: force a minimum of 15 rows in plot space
+    n_hits <- if (has_data) nrow(orfs) else 0
+    view_limit <- max(12, n_hits) 
+    
+    plot(
+      x = 1, type = "n",
+      xlim = c(0, bl$V3[1]),
+      ylim = c(0, view_limit + 1),
+      col = "white", yaxt = "n", main = "",
+      xlab = "TE consensus structure and protein hits (bp)",
+      ylab = "", cex.lab = 1.5, cex.axis = 1.2, xaxs = "i", yaxs = "i"
+    )
+    
+    if (!has_data) {
+      text("No TE domain detected", x = bl$V3[1] / 2, y = view_limit / 2, cex = 2)
+    } else {
+      for (i in 1:nrow(orfs)) {
+        # Scaling: place hits from the top down
+        y_pos <- (view_limit - i + 1) * row_height
+        
+        orf_color <- if (orfs$V1[i] < orfs$V2[i]) "black" else "red"
+        
+        # Outer Frame
+        rect(
+          xleft = orfs$V1[i], xright = orfs$V2[i],
+          ybottom = y_pos - (bar_thickness / 2),
+          ytop = y_pos + (bar_thickness / 2),
+          lwd = 1.2, border = orf_color
+        )
+        
+        # Protein Hit Fill
+        rect(
+          xleft = orfs$V5[i], xright = orfs$V6[i],
+          ybottom = y_pos - (bar_thickness / 2.5),
+          ytop = y_pos + (bar_thickness / 2.5),
+          col = paste0("#", orfs$V9[i]), border = NA
+        )
+        
+        # Label
+        text(
+          paste0("(", orfs$V8[i], ") ", orfs$V3[i], " ", orfs$V4[i]),
+          x = (orfs$V1[i] + orfs$V2[i]) / 2,
+          y = y_pos + (bar_thickness / 1.5),
+          pos = 3, cex = 1.1
+        )
+      }
+    }
+  }
+  
+  plot_te_structure(orfs, bl)
+  
+  if (tables == "TRUE") {
+    names(bl) <- c("TE", "from.1", "to.1", "from.2", "to.2")
+    write.table(bl, file = paste0(output, "/TE.self-blast.txt"), quote = FALSE, row.names = FALSE)
+  }
+}
--- tetrimmer/TE-Aid-master/consensus2genome.R
+++ tetrimmer/TE-Aid-master/consensus2genome.R
@@ -10,88 +10,157 @@
 ### query: path to query (fasta file)                                               ###
 ### db: path to blast db (blast formated nucleotide database)                       ###
 #######################################################################################
-# Changelog V2 --> V3 | 04.19.2021
-# - Fit for shell wrapper, remove regions highlight from usused trim script
-# Changelog V1 --> V2 | 03.13.2020
-# - Add a second graph with suggested cut points
-# 
-# V2=alpha | not for release
-# TO FIX: trace the coverage on the left graph
-# TO DO: convert breakpoints to bed for getfasta
 
-
-
-consensus2genome=function(query=NULL, db=NULL, evalue=10e-8, FL_thresh=0.9, alpha=0.3, full_alpha=1, auto_y=T, bins=NULL, output = NULL, v_x_line_1 = 0, v_x_line_2 = 0){
-  if(is.null(query)){print('query not specified')}
-  if(is.null(db)){print('db not specified')}
-  #perform the blast
-  blast=read.table(text=system(paste("blastn -max_target_seqs 10000 -query", query, "-db", db , "-evalue", evalue, "-outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send sstrand evalue qcovhsp' | sed 's/#/-/g'"), intern = TRUE))
+consensus2genome <- function(query = NULL,
+                             db = NULL,
+                             evalue = 10e-8,
+                             FL_thresh = 0.9,
+                             alpha = 0.5,
+                             full_alpha = 1,
+                             auto_y = TRUE,
+                             bins = NULL,
+                             output = NULL,
+                             v_x_line_1 = 0,
+                             v_x_line_2 = 0) {
+  
+  if (is.null(query)) { print('query not specified') }
+  if (is.null(db)) { print('db not specified') }
+  
+  # Perform the blast
+  blast <- read.table(
+    text = system(
+      paste(
+        "blastn -max_target_seqs 10000 -query", query, "-db", db, "-evalue", evalue,
+        "-outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send sstrand evalue qcovhsp' | sed 's/#/-/g'"
+      ),
+      intern = TRUE
+    )
+  )
+  
   # Write BLAST results to the output directory
   output_filepath <- file.path(output, "blastn.txt")
   write.table(blast, file = output_filepath, quote = FALSE, row.names = FALSE)
-
-  #TE consensus size
-  cons_len=as.numeric(system(paste(bins,"/getlength.sh ",query, sep = ""), intern = TRUE))
-  print(paste("consensus length: ", cons_len, "bp", sep = " "))
-  #list of almost full length fragments
-  full=blast[abs(blast$V7-blast$V8) >= FL_thresh*as.numeric(cons_len),]
-  #graph
-  if(auto_y == T){
-    #par(mar=c(2,2,2,2))
-    plot(range(0, cons_len), range(0, max(100-blast$V3)), type = "n", main=paste("TE: ", as.character(blast[1,1]), "\n size: ", as.character(cons_len), "bp; fragments: ", as.character(length(blast$V1)), "; full length: ", as.character(length(full$V1))," (>=",as.character(as.numeric(FL_thresh)*cons_len),"bp)", sep = ""), cex.main = 2, xlab = "TE consensus (bp)", ylab = "divergence to consensus (%)", xaxs = "i", yaxs = "i", cex.lab = 2, cex.axis = 1.5)
-
-    # add vertical red line
-    if (v_x_line_1 > 0){
-      abline(v = v_x_line_1, col = "blue", lwd = 2)
-    }
-
-    if (v_x_line_2 > 0){
-      abline(v = v_x_line_2, col = "blue", lwd = 2)
-    }
-
-    for(i in 1:length(blast$V1)){
-      segments(blast$V7[i], 100-blast$V3[i], blast$V8[i], 100-blast$V3[i], col=rgb(0,0,0,alpha=alpha))
-    }
-    for(i in 1:length(blast$V1)){
-      segments(full$V7[i], 100-full$V3[i], full$V8[i], 100-full$V3[i], col=rgb(1,0,0, alpha=full_alpha), lwd=1.5)
-    }}
-  else{
-    #par(mar=c(2,2,2,2))
-    plot(range(0, cons_len), range(0, auto_y), type = "n", main=paste("TE: ", as.character(blast[1,1]), "\n size: ", as.character(cons_len), "bp; fragments: ", as.character(length(blast$V1)), "; full length: ", as.character(length(full$V1))," (>=",as.character(as.numeric(FL_thresh)*cons_len),"bp)", sep = ""), cex.main = 2, xlab = "TE consensus (bp)", ylab = "divergence to consensus (%)", cex.lab = 2, cex.axis = 1.5)
-    for(i in 1:length(blast$V1)){
-      segments(blast$V7[i], 100-blast$V3[i], blast$V8[i], 100-blast$V3[i], col=rgb(0,0,0,alpha=alpha))
-    }
-    for(i in 1:length(blast$V1)){
-      segments(full$V7[i], 100-full$V3[i], full$V8[i], 100-full$V3[i], col=rgb(1,0,0, alpha=full_alpha), lwd=1.5)
-    }}
   
-#make the coverage matrix and graph
-coverage=matrix(rep(0, length(blast$V1)*as.numeric(cons_len)), byrow = T, ncol = as.numeric(cons_len))
-for(i in 1:length(blast$V1)){
-    coverage[i,]<-c(rep(0,blast$V7[i]-1),rep(1,abs(blast$V8[i]-blast$V7[i])+1), rep(0,as.numeric(cons_len)-blast$V8[i]))
+  # TE consensus size
+  cons_len <- as.numeric(system(paste(bins, "/getlength.sh ", query, sep = ""), intern = TRUE))
+  print(paste("consensus length: ", cons_len, "bp", sep = " "))
+  
+  if (v_x_line_1 > 0 && v_x_line_2 > 0) {
+    real_cons_len <- abs(as.numeric(v_x_line_2) - as.numeric(v_x_line_1))
+  } else {
+    real_cons_len <- cons_len
+  }
+  
+  # List half full of length fragments
+  half_full <- blast[0.5 * as.numeric(real_cons_len) <= abs(blast$V7 - blast$V8) & 
+                       abs(blast$V7 - blast$V8) < FL_thresh * as.numeric(real_cons_len), ]
+  
+  # List of almost full length fragments
+  full <- blast[abs(blast$V7 - blast$V8) >= FL_thresh * as.numeric(real_cons_len), ]
+  
+  # List blast hit for grey color
+  blast_grey <- blast[abs(blast$V7 - blast$V8) < 0.5 * as.numeric(real_cons_len), ]
+  
+  # Graph
+  if (auto_y == TRUE) {
+    plot(
+      range(0, cons_len),
+      range(0, max(100 - blast$V3)),
+      type = "n",
+      main = paste(
+        "TE: ", as.character(blast[1, 1]),
+        "\n size: ", as.character(cons_len),
+        "bp; fragments: ", as.character(length(blast$V1)),
+        "; full length: ", as.character(length(full$V1)),
+        " (>=", as.character(as.numeric(FL_thresh) * cons_len), "bp)",
+        sep = ""
+      ),
+      cex.main = 2,
+      xlab = "TE consensus (bp)",
+      ylab = "divergence to consensus (%)",
+      xaxs = "i",
+      yaxs = "i",
+      cex.lab = 2,
+      cex.axis = 1.5
+    )
+    
+    # Add vertical blue lines
+    if (v_x_line_1 > 0) { abline(v = v_x_line_1, col = "blue", lwd = 2) }
+    if (v_x_line_2 > 0) { abline(v = v_x_line_2, col = "blue", lwd = 2) }
+    
+    for (i in 1:length(blast$V1)) {
+      segments(blast_grey$V7[i], 100 - blast_grey$V3[i], blast_grey$V8[i], 100 - blast_grey$V3[i], col = rgb(0, 0, 0, alpha = alpha))
+    }
+    
+    for (i in 1:length(blast$V1)) {
+      segments(half_full$V7[i], 100 - half_full$V3[i], half_full$V8[i], 100 - half_full$V3[i], col = rgb(0, 0.7, 0.1, alpha = 0.8), lwd = 1.1)
+    }
+    
+    for (i in 1:length(blast$V1)) {
+      segments(full$V7[i], 100 - full$V3[i], full$V8[i], 100 - full$V3[i], col = rgb(1, 0, 0, alpha = full_alpha), lwd = 1.3)
+    }
+    
+  } else {
+    plot(
+      range(0, cons_len),
+      range(0, auto_y),
+      type = "n",
+      main = paste(
+        "TE: ", as.character(blast[1, 1]),
+        "\n size: ", as.character(cons_len),
+        "bp; fragments: ", as.character(length(blast$V1)),
+        "; full length: ", as.character(length(full$V1)),
+        " (>=", as.character(as.numeric(FL_thresh) * cons_len), "bp)",
+        sep = ""
+      ),
+      cex.main = 2,
+      xlab = "TE consensus (bp)",
+      ylab = "divergence to consensus (%)",
+      cex.lab = 2,
+      cex.axis = 1.5
+    )
+    
+    for (i in 1:length(blast$V1)) {
+      segments(blast$V7[i], 100 - blast$V3[i], blast$V8[i], 100 - blast$V3[i], col = rgb(0, 0, 0, alpha = alpha))
+    }
+    
+    for (i in 1:length(blast$V1)) {
+      segments(full$V7[i], 100 - full$V3[i], full$V8[i], 100 - full$V3[i], col = rgb(1, 0, 0, alpha = full_alpha), lwd = 1.5)
+    }
+  }
+  
+  # --- Optimized Coverage Calculation ---
+  cons_len_num <- as.numeric(cons_len)
+  cov_vector <- rep(0, cons_len_num)
+  
+  for (i in 1:nrow(blast)) {
+    start <- max(1, min(blast$V7[i], blast$V8[i]))
+    end <- min(cons_len_num, max(blast$V7[i], blast$V8[i]))
+    
+    if (start <= end) {
+      cov_vector[start:end] <- cov_vector[start:end] + 1
+    }
+  }
+  
+  # --- Updated removator function ---
+  removator <- function(covV) {
+    plot(
+      covV,
+      type = "l",
+      main = "",
+      xaxs = "i",
+      yaxs = "i",
+      xlab = "TE consensus genomic coverage plot (bp)",
+      ylab = "coverage (bp)",
+      cex.lab = 2,
+      cex.axis = 1.5,
+      ylim = c(0, max(covV, na.rm = TRUE) * 1.1)
+    )
+    
+    if (as.numeric(v_x_line_1) > 0) { abline(v = as.numeric(v_x_line_1), col = "blue", lwd = 2) }
+    if (as.numeric(v_x_line_2) > 0) { abline(v = as.numeric(v_x_line_2), col = "blue", lwd = 2) }
+  }
+  
+  # --- Execution ---
+  removator(cov_vector)
 }
-
-    # TO FIX: trace the coverage on the left graph
-    #points(colSums(coverage), type='l', axes = F, ylab = NA, xlab = NA, col=covcol, ylim = c(0, max(colSums(coverage))))
-    #axis(side = 4)
-    #mtext(side = 4, line = 3, 'consensus coverage (bp)')
-    
-    ## import removator
-    
-    removator<-function(covM){
-      as.data.frame(covM)->covMT
-      covMT$bp=rownames(covMT)
-      plot(covM, type = "l", main = "", xaxs = "i", yaxs = "i",  xlab = "TE consensus genomic coverage plot (bp)", ylab = "coverage (bp)", cex.lab = 2, cex.axis = 1.5, ylim = c(0, max(covM, na.rm = TRUE)))
-
-      # add vertical red line
-      if (v_x_line_1 > 0){
-        abline(v = v_x_line_1, col = "blue", lwd = 2)
-      }
-
-      if (v_x_line_2 > 0){
-        abline(v = v_x_line_2, col = "blue", lwd = 2)
-      }
-    } # removator function
-
-    removator(colSums(coverage)) # makes the second graph
-}# whole funtion
